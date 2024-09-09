d9bf90cfa5eddd0c26f5ef5a6e45ba59022d4835
magr4826 <magr4826@users.noreply.github.com>
2024-09-09 14:17:24 +0000

    chore: accept new Cruft update

--- move_template_files.sh
+++ move_template_files.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-rsync -avh nomad-parser-yambospectra/ .
-rm -rfv nomad-parser-yambospectra
+if ! command -v rsync &> /dev/null; then
+  echo "rsync required, but not installed!"
+  exit 1
+else
+  rsync -avh nomad-parser-yambospectra/ .
+  rm -rfv nomad-parser-yambospectra
+fi
